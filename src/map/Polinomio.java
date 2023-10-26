dfc1dd06f26f4858b5dca8418dd5014994d7aeb9
Sergio Aramburu <sergioaramburu@tutanota.com>
2023-10-26 10:12:45 +0200

    feat: create Polinomio class

---
+++ src/map/Polinomio.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package map;
+
+/**
+ *
+ * @author seram
+ */
+public class Polinomio {
+    private Map polinomio;
+            
+    public Polinomio() {
+        polinomio = new HashMap<>();
+    }
+}
