25f3117f45c12b144e73d1c75721bbfa72f8f220
Sergio Aramburu <sergioaramburu@tutanota.com>
2023-11-07 16:35:47 +0100

    feat(Vertice): Vertice class

---
+++ src/map/Vertice.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package map;
+
+/**
+ *
+ * @author seram
+ */
+public class Vertice<E> {
+
+    private E etiqueta;
+
+    public Vertice(E etiqueta) {
+        this.etiqueta = etiqueta;
+    }
+
+    public E getEtiqueta() {
+        return etiqueta;
+    }
+
+}
